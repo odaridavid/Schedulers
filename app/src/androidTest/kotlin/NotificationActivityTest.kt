d9944430b85fa23548c66b175ba6d992118bbd19
Davidodari <davidkibzodari@gmail.com>
2019-12-26 17:35:56 +0300

    test notification displayed

--- app/src/androidTest/kotlin/NotificationActivityTest.kt
+++ app/src/androidTest/kotlin/NotificationActivityTest.kt
@@ -4,7 +4,11 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.Until
 import com.dari.schedulers.R
 import com.dari.schedulers.notifications.NotificationActivity
 import org.junit.Test
@@ -22,5 +26,8 @@ class NotificationActivityTest {
         activityTestRule.launchActivity(intent)
         SystemClock.sleep(1000)
         onView(withId(R.id.send_notification_button)).perform(click())
+        val uiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+        uiDevice.openNotification()
+        uiDevice.wait(Until.hasObject(By.textStartsWith("Schedulers")), 2000)
     }
 }
